37b192c56df564d16c4c4db83b67ec951b96c87e
Vivian Tanaka <tanakavv15@gmail.com>
2021-07-08 22:55:32 -0300

    Project hr-api-zull-gateway created

---
+++ hr-api-zuul-gateway/src/main/java/com/devsuperior/hrapizuulgateway/HrApiZuulGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrapizuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrApiZuulGatewayApplication.class, args);
+    }
+
+}
